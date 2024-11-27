6cbc3360f3b45a7ca88dbd7ed53dc2c2479cd8a5
LeandroRocha <leandrorochaadm@gmail.com>
2024-11-27 16:59:07 -0300

    create AppColors

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+  static const bluePrimary = Color(0xFF0500FF);
+  static const blue10 = Color(0xFFEDEFFB);
+  static const black = Color(0xFF1C1C1C);
+  static const gray20 = Color(0xFF9E9E9E);
+  static const gray10 = Color(0xFFDFDFDF);
+  static const gray05 = Color(0xFFF5F5F5);
+  static const gray00 = Color(0xFFF0F0F0);
+  static const white = Color(0xFFFFFFFF);
+}
